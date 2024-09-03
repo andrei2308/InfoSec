9eabcf37487a760518221bea2fcb9a812d5c4a6d
andrei2308 <chitoiu.andrei2@yahoo.com>
2024-09-03 18:16:56 +0300

    Hide powered by

--- myApp.js
+++ myApp.js
@@ -1,54 +1,8 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
 const api = require('./server.js');
+const helmet = require('helmet');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
@@ -59,3 +13,50 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+app.disable('x-powered-by');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
