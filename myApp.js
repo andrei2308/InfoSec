40282941ef95645c3f4ccdbf0270374036e89f63
andrei2308 <chitoiu.andrei2@yahoo.com>
2024-09-03 18:20:28 +0300

    hide express

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-app.disable('x-powered-by');
+app.use(helmet.hidePoweredBy())
 
 
 
