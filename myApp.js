22986a02f1acb91e1ce49a3346771a025dc7095f
andrei2308 <chitoiu.andrei2@yahoo.com>
2024-09-03 18:38:14 +0300

    express fix 2

--- myApp.js
+++ myApp.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
-const app = express();
-const api = require('./server.js');
+const express = require('express')
+const helmet = require('helmet')
+const app = express()
 
-app.use(helmet());
-app.use(helmet.hidePoweredBy()); 
+app.use(helmet.hidePoweredBy())
 
-app.use(express.static('public'));
 
-app.use('/_api', api);
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=app
+const api = require('./server.js')
+app.use(express.static('public'))
+app.disable('strict-transport-security')
+app.use('/_api',api)
+app.get("/",function (request ,response) {
+response.sendFile(__dirname + '/views/index.html');
 });
-
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
- app.disable('strict-transport-security');
+let port = process.env.port || 3000
+app.listen(port,()=>{
+console.log(`Server started on port ${port}`)
+})
